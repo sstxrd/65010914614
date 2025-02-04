aa833f3bfb9d9c96fa80d73ffb41f06cde5c085a
sstxrd <142972311+sstxrd@users.noreply.github.com>
2025-02-04 11:53:09 +0700

    Update connectdb.php
    rename

--- i/connectdb.php
+++ i/connectdb.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pwd = "";
-$db = "shop";
+$pwd = "12345678P";
+$db = "shop4614";
 
 $conn = mysqli_connect($host,$user,$pwd)or die ("cannot connection");
 mysqli_select_db($conn,$db)or die ("cannot select database");
